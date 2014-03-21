bdaf074d8fa20c845c404be95ed72571ba1ce954
Peter <peter@peter-Inspiron-N5050.(none)>
2014-03-21 01:14:54 -0700

    views modified and created

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -95,7 +95,7 @@ SimpleForm.setup do |config|
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
 
   # You can define the class to use on all labels. Default is nil.
-  config.label_class = 'control-label'
+  config.label_class = ''
 
   # You can define the class to use on all forms. Default is simple_form.
   # config.form_class = :simple_form
@@ -142,4 +142,5 @@ SimpleForm.setup do |config|
 
   # Default class for inputs
   # config.input_class = nil
+  config.input_class = 'form-control'
 end
